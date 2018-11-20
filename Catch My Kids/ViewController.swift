15b5e669f840bdfede1ed57ee2c02cdfb75ffe76
Kafkas Baranseli <kafkasbaranseli@kafkass-imac.local>
2018-11-20 13:49:19 +0300

    Initial Commit

---
+++ Catch My Kids/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Catch My Kids
+//
+//  Created by Kafkas Baranseli on 20/11/2018.
+//  Copyright © 2018 Baranseli Financial Settlements. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
